e49a7a150686570c17bb96b38463400cc0eca160
孙旭 <sunxucxm@163.com>
2019-06-03 15:43:01 +0800

    忽略

---
+++ DesignModel/DesignModel/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  DesignModel
+//
+//  Created by 孙旭 on 2019/6/3.
+//  Copyright © 2019 sunxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+///////33333
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
